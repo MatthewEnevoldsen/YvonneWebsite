31781b797ec463d7d46581390bb8892c0ebd3735
Matthew David <MattEne@hotmail.co.uk>
2015-05-11 22:39:35 +0100

    Something happens!

---
+++ YvonneFengSite/YvonneFengSite/app/Biography/biographyController.ts
@@ -0,0 +1,40 @@
+﻿module Biography {
+    export class Controller {
+        static $inject = ['$scope'];
+
+
+        constructor(private $scope: BiographyScope) {
+            $scope.sections = this.getBio();
+        }
+
+        private getBio = function (): Array<BioSection> {
+            return [
+                new BioSection("Bio1", [
+                    new BioEvent("1989", "Born"),
+                    new BioEvent("1990", "1st Birthday")
+                ]),
+                new BioSection("Schoole", [
+                    new BioEvent("1995", "BabbySchool"),
+                    new BioEvent("2015", "Rambled about some word")
+                ])
+            ];
+        }
+    }
+
+    class BioSection {
+        constructor(
+            public heading: string,
+            public events: Array<BioEvent>) { }
+    }
+
+    class BioEvent {
+        constructor(
+            public date: string,
+            public description: string) { }
+    }
+
+    interface BiographyScope extends ng.IScope {
+        sections: BioSection[];
+    }
+
+}
---
+++ YvonneFengSite/YvonneFengSite/app/Contact/contactController.ts
@@ -0,0 +1,13 @@
+﻿module Contact {
+    export class Controller {
+        static $inject = ['$scope'];
+
+        constructor(private $scope: ContactScope) {
+            $scope.message = "SomeEmail";
+        }
+    }
+
+    interface ContactScope extends ng.IScope {
+        message: string
+    }
+}
