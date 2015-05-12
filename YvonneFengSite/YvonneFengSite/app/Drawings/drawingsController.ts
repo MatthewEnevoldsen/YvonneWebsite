839a525a2fb0dd52ae03d0d026c136c8988bf112
Matthew David <MattEne@hotmail.co.uk>
2015-05-12 23:01:32 +0100

    It displays pictures

---
+++ YvonneFengSite/YvonneFengSite/app/Drawings/drawingsController.ts
@@ -0,0 +1,28 @@
+﻿module Drawings {
+    export class Controller {
+        static $inject = ['$scope'];
+
+
+
+        constructor($scope: DrawingsScope) {
+            var fileList: string[] = [
+                "Resources/Drawings/large/10pm, 2014, ink on paper, 53 x 38 cm.jpg",
+                "Resources/Drawings/large/C0415, 2014, ink on Chinese paper, 45.5 x 69.5.jpg",
+                "Resources/Drawings/large/Chop, 2014, ink on paper, 42 x 29.7 cm.jpg",
+                "Resources/Drawings/large/Hand, ink on paper, 42 x 29.7 cm.jpg",
+                "Resources/Drawings/large/Scan, charcoal on paper, 38 x 26 cm.jpg",
+                "Resources/Drawings/large/Sit, 2014, ink on paper, 42 x 29.7 cm.jpg",
+                "Resources/Drawings/large/You are one of us, 2014, ink on Chinese paper, 45.5 x 69.5cm.",
+                "Resources/Drawings/large/Sleepless five years ahead, 2014, ink on paper, 53 x 38cm.jpg",
+                "Resources/Drawings/large/Spray, ink on paper, 42 x 29.7 cm.jpg",
+                "Resources/Drawings/large/Unblock, 2014, ink on paper, 53 x 38 cm.jpg",
+                "Resources/Drawings/large/_1.jpg"
+            ];
+            $scope.pictures = fileList;
+        }
+    }
+
+    interface DrawingsScope extends ng.IScope {
+        pictures: string[];
+    }
+}
---
+++ YvonneFengSite/YvonneFengSite/app/News/newsController.ts
@@ -0,0 +1,13 @@
+﻿module News {
+    export class Controller {
+        static $inject = ['$scope'];
+
+        constructor(private $scope: NewsScope) {
+            $scope.message = "Yvonne.wyf@gmail.com";
+        }
+    }
+
+    interface NewsScope extends ng.IScope {
+        message: string
+    }
+}
