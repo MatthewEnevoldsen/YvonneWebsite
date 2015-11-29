2c3afbb4c2eb1806f66d05138c5a9f2f58b8e3d6
Matthew Enevoldsen <mattene@hotmail.co.uk>
2015-11-29 01:03:35 +0000

    Added work I image suite

--- YvonneFengSite/YvonneFengSite/app/Images/imageController.ts
+++ YvonneFengSite/YvonneFengSite/app/Images/imageController.ts
@@ -14,72 +14,51 @@
                 {
                     name: "Drawings",
                     images: [
-                        {
-                            id: 0,
-                            path: "Resources/Drawings/large/10pm, 2014, ink on paper, 53 x 38 cm.jpg",
-                            name: "10pm",
-                            year: 2014,
-                            medium: "ink on paper",
-                            width: 53,
-                            height: 38
-                        },
-                        {
-                            id: 1,
-                            path: "Resources/Drawings/large/C0415, 2014, ink on Chinese paper, 45.5 x 69.5.jpg",
-                            name: "C0415",
-                            year: 2014,
-                            medium: "ink on Chinese paper",
-                            width: 45.5,
-                            height: 69.5
-                        },
+                        { id: 0, name: "Delete", year: 2015, medium: "Ink on paper", size: "29.7x42cm", path: "Resources/Drawings/I/Delete.jpg" },
+                        { id: 1, name: "You are one of us", year: 2014, medium: "Ink on Chinese paper", size: "69.5x45.5cm", path: "Resources/Drawings/I/You are one of us.jpg" },
+                        { id: 2, name: "C0415", year: 2014, medium: "Ink on Chinese paper", size: "69.5x45.5cm", path: "Resources/Drawings/I/C0415.jpg" },
+                        { id: 3, name: "Sleepless five years ahead", year: 2014, medium: "Ink on paper", size: "53x38cm", path: "Resources/Drawings/I/Sleepless five years ahead.jpg" },
+                        { id: 4, name: "Hand", year: 2014, medium: "Ink on paper", size: "42x29.7cm", path: "Resources/Drawings/I/Hand.jpg" },
+                        { id: 5, name: "10pm", year: 2014, medium: "Ink on paper", size: "53x38cm", path: "Resources/Drawings/I/10pm.jpg" },
+                        { id: 6, name: "Unblock", year: 2014, medium: "Ink on paper", size: "53x38cm", path: "Resources/Drawings/I/Unblock.jpg" },
+                        { id: 7, name: "Sit", year: 2014, medium: "Ink on paper", size: "29.7x42cm", path: "Resources/Drawings/I/Sit.jpg" },
                     ]
                 },
                 {
-                    name: "Sketchbook",
+                    name: "Sketches",
                     images: [
-                        {
-                            id: 0,
-                            path: "Resources/Drawings/large/10pm, 2014, ink on paper, 53 x 38 cm.jpg",
-                            name: "10pm",
-                            year: 2014,
-                            medium: "ink on paper",
-                            size: "53x38cm",
-                            width: 53,
-                            height: 38
-                        },
-                        {
-                            id: 1,
-                            path: "Resources/Drawings/large/C0415, 2014, ink on Chinese paper, 45.5 x 69.5.jpg",
-                            name: "C0415",
-                            year: 2014,
-                            medium: "ink on Chinese paper",
-                            width: 45.5,
-                            height: 69.5
-                        },
+                        { id: 0, name: "Sketchbook project, commission by Onassis Cultural Centre, Greece", year: 2015, medium: "Ink on paper", size: "A4 sketchbook", path: "Resources/drawings/sketchbook/1.jpg" },
+                        { id: 1, name: "Sketchbook project, commission by Onassis Cultural Centre, Greece", year: 2015, medium: "Ink on paper", size: "A4 sketchbook", path: "Resources/drawings/sketchbook/2.jpg" },
+                        { id: 2, name: "Sketchbook project, commission by Onassis Cultural Centre, Greece", year: 2015, medium: "Ink on paper", size: "A4 sketchbook", path: "Resources/drawings/sketchbook/3.jpg" },
+                        { id: 3, name: "Sketchbook project, commission by Onassis Cultural Centre, Greece", year: 2015, medium: "Ink on paper", size: "A4 sketchbook", path: "Resources/drawings/sketchbook/4.jpg" },
+                        { id: 4, name: "Sketchbook project, commission by Onassis Cultural Centre, Greece", year: 2015, medium: "Ink on paper", size: "A4 sketchbook", path: "Resources/drawings/sketchbook/5.jpg" },
+                        { id: 5, name: "Sketchbook project, commission by Onassis Cultural Centre, Greece", year: 2015, medium: "Ink on paper", size: "A4 sketchbook", path: "Resources/drawings/sketchbook/6.jpg" },
+                        { id: 6, name: "Sketchbook project, commission by Onassis Cultural Centre, Greece", year: 2015, medium: "Ink on paper", size: "A4 sketchbook", path: "Resources/drawings/sketchbook/7.jpg" },
                     ]
                 },
                 {
                     name: "WorkI",
                     images: [
-                        { id: 1, name: "Final moment", year: 2015, medium: "Charcoal,  ink and oil on canvas", size: "101.6x76.2cm", path: "Resources/WorkI/Final moment.jpg" },
-                        { id: 2, name: "Panegyric", year: 2015, medium: "Charcoal and acrylic on canvas", size: "160x200cm", path: "Resources/WorkI/Panegyric.jpg" },
-                        { id: 3, name: "Don’t come back", year: 2014, medium: "Charcoal and acrylic on canvas", size: "200x150cm", path: "Resources/WorkI/Don't come back.jpg" },
-                        { id: 4, name: "Must it be? It must be!", year: 2014, medium: "Charcoal and acrylic on canvas", size: "160x200cm", path: "Resources/WorkI/Must it be It must be.jpg" },
-                        { id: 5, name: "Video game", year: 2014, medium: "Charcoal and acrylic on canvas", size: "150x185cm", path: "Resources/WorkI/Video game.jpg" },
-                        { id: 6, name: "Mammogram", year: 2014, medium: "Charcoal and acrylic on canvas", size: "155x185cm", path: "Resources/WorkI/Mammogram.jpg" },
-                        { id: 7, name: "The phone", year: 2013, medium: "Acrylic on canvas", size: "180x150cm", path: "Resources/WorkI/The phone.jpg" }
+                        { id: 0, name: "Plaster/Phone", year: 2015, medium: "Oil on polyester and canvas", size: "101.5x61cm each", path: "Resources/WorkI/PlasterPhone.jpg" },
+                        { id: 1, name: "Shadow", year: 2015, medium: "Charcoal, acrylic and oil on canvas ", size: "130x170cm", path: "Resources/WorkI/Shadow.jpg" },
+                        { id: 2, name: "Jade bracelet", year: 2015, medium: "Oil on board ", size: "40x60cm", path: "Resources/WorkI/Jade bracelet.jpg" },
+                        { id: 3, name: "Trace of cracks", year: 2015, medium: "Charcoal, acrylic and newsprint on canvas", size: "110x150cm", path: "Resources/WorkI/Trace of cracks.jpg" },
+                        { id: 4, name: "Immersive darkness", year: 2015, medium: "Charcoal, graphite and acrylic on canvas", size: "120x120cm", path: "Resources/WorkI/Immersive darkness.jpg" },
+                        { id: 5, name: "Fence", year: 2015, medium: "Charcoal and ink on paper", size: "150x200cm each", path: "Resources/WorkI/Fence.jpg" },
+                        { id: 6, name: "Suite", year: 2015, medium: "Ink, acrylic and oil on canvas", size: "91.5x121.5cm", path: "Resources/WorkI/Suite.jpg" },
+                        { id: 7, name: "Step to the left, step to the right", year: 2015, medium: "Oil and oil pastel on canvas", size: "101.6x76.2cm", path: "Resources/WorkI/Step to the left step to the right.jpg" },
                     ]
                 },
                 {
                     name: "WorkII",
                     images: [
-                        { id: 1, name: "Final moment", year: 2015, medium: "Charcoal,  ink and oil on canvas", size: "101.6x76.2cm", path: "Resources/WorkI/Final moment.jpg" },
-                        { id: 2, name: "Panegyric", year: 2015, medium: "Charcoal and acrylic on canvas", size: "160x200cm", path: "Resources/WorkI/Panegyric.jpg" },
-                        { id: 3, name: "Don’t come back", year: 2014, medium: "Charcoal and acrylic on canvas", size: "200x150cm", path: "Resources/WorkI/Don't come back.jpg" },
-                        { id: 4, name: "Must it be? It must be!", year: 2014, medium: "Charcoal and acrylic on canvas", size: "160x200cm", path: "Resources/WorkI/Must it be It must be.jpg" },
-                        { id: 5, name: "Video game", year: 2014, medium: "Charcoal and acrylic on canvas", size: "150x185cm", path: "Resources/WorkI/Video game.jpg" },
-                        { id: 6, name: "Mammogram", year: 2014, medium: "Charcoal and acrylic on canvas", size: "155x185cm", path: "Resources/WorkI/Mammogram.jpg" },
-                        { id: 7, name: "The phone", year: 2013, medium: "Acrylic on canvas", size: "180x150cm", path: "Resources/WorkI/The phone.jpg" }
+                        { id: 0, name: "Final moment", year: 2015, medium: "Charcoal,  ink and oil on canvas", size: "101.6x76.2cm", path: "Resources/WorkI/Final moment.jpg" },
+                        { id: 1, name: "Panegyric", year: 2015, medium: "Charcoal and acrylic on canvas", size: "160x200cm", path: "Resources/WorkI/Panegyric.jpg" },
+                        { id: 2, name: "Don’t come back", year: 2014, medium: "Charcoal and acrylic on canvas", size: "200x150cm", path: "Resources/WorkI/Don't come back.jpg" },
+                        { id: 3, name: "Must it be? It must be!", year: 2014, medium: "Charcoal and acrylic on canvas", size: "160x200cm", path: "Resources/WorkI/Must it be It must be.jpg" },
+                        { id: 4, name: "Video game", year: 2014, medium: "Charcoal and acrylic on canvas", size: "150x185cm", path: "Resources/WorkI/Video game.jpg" },
+                        { id: 5, name: "Mammogram", year: 2014, medium: "Charcoal and acrylic on canvas", size: "155x185cm", path: "Resources/WorkI/Mammogram.jpg" },
+                        { id: 6, name: "The phone", year: 2013, medium: "Acrylic on canvas", size: "180x150cm", path: "Resources/WorkI/The phone.jpg" },
                     ]
                 }
             ];
